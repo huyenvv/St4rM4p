01780b58a1951785e1cfbc99f87b3160330b1b30
HuyenVV <huyenvv@fsoft.com.vn>
2015-12-02 15:02:41 +0700

    Commit all code

---
+++ StarMap/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using StarMap.Helpers;
+using StarMap.Models;
+
+namespace StarMap.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Person per)
+        {
+            return View();
+        }
+
+        public ActionResult SetCulture(string culture)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+
+            // Save culture in a cookie
+            var cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+
+                cookie = new HttpCookie("_culture")
+                {
+                    Value = culture,
+                    Expires = DateTime.Now.AddYears(1)
+                };
+            }
+            Response.Cookies.Add(cookie);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
